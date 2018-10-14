7a7c3f0cc9cd6e401a3356c36c59e99543337452
maria-angslz <maria.angslz@gmail.com>
2018-10-14 17:11:26 -0300

    menu principal

---
+++ IG/GUI.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 13 18:26:29 2018
+
+@author: sho_m
+"""
+
+import sys
+from PyQt5 import uic, QtWidgets
+
+qtCreatorFile = "VentanaPrincipal.ui" # Nombre del archivo aquí.
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app =  QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
