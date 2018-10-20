2a91c13d84cef23759230ecc079171f13739fe45
maria-angslz <maria.angslz@gmail.com>
2018-10-19 22:01:13 -0300

    ventana dimension

---
+++ IG/Dimensionador.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Dimensionador.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dimensionador(object):
+    def setupUi(self, Dimensionador):
+        Dimensionador.setObjectName("Dimensionador")
+        Dimensionador.resize(557, 199)
+        Dimensionador.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.centralwidget = QtWidgets.QWidget(Dimensionador)
+        self.centralwidget.setObjectName("centralwidget")
+        self.Filas = QtWidgets.QLabel(self.centralwidget)
+        self.Filas.setGeometry(QtCore.QRect(40, 70, 81, 16))
+        self.Filas.setObjectName("Filas")
+        self.Columnas = QtWidgets.QLabel(self.centralwidget)
+        self.Columnas.setGeometry(QtCore.QRect(30, 100, 101, 16))
+        self.Columnas.setObjectName("Columnas")
+        self.buttonBox = QtWidgets.QDialogButtonBox(self.centralwidget)
+        self.buttonBox.setGeometry(QtCore.QRect(190, 140, 161, 31))
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(150, 30, 381, 91))
+        self.widget.setObjectName("widget")
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.NumeroFilasB_2 = QtWidgets.QSpinBox(self.widget)
+        self.NumeroFilasB_2.setObjectName("NumeroFilasB_2")
+        self.gridLayout.addWidget(self.NumeroFilasB_2, 1, 2, 1, 1)
+        self.NumeroColumnasA = QtWidgets.QSpinBox(self.widget)
+        self.NumeroColumnasA.setObjectName("NumeroColumnasA")
+        self.gridLayout.addWidget(self.NumeroColumnasA, 2, 0, 1, 1)
+        self.NumeroFilasX = QtWidgets.QSpinBox(self.widget)
+        self.NumeroFilasX.setObjectName("NumeroFilasX")
+        self.gridLayout.addWidget(self.NumeroFilasX, 1, 1, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.widget)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 0, 1, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.widget)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_3.setFont(font)
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 0, 2, 1, 1)
+        self.NumeroFilasA = QtWidgets.QSpinBox(self.widget)
+        self.NumeroFilasA.setObjectName("NumeroFilasA")
+        self.gridLayout.addWidget(self.NumeroFilasA, 1, 0, 1, 1)
+        self.label = QtWidgets.QLabel(self.widget)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        Dimensionador.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(Dimensionador)
+        self.statusbar.setObjectName("statusbar")
+        Dimensionador.setStatusBar(self.statusbar)
+
+        self.retranslateUi(Dimensionador)
+        QtCore.QMetaObject.connectSlotsByName(Dimensionador)
+
+    def retranslateUi(self, Dimensionador):
+        _translate = QtCore.QCoreApplication.translate
+        Dimensionador.setWindowTitle(_translate("Dimensionador", "MainWindow"))
+        self.Filas.setText(_translate("Dimensionador", "Numero de filas"))
+        self.Columnas.setText(_translate("Dimensionador", "Numero de columnas"))
+        self.label_2.setText(_translate("Dimensionador", "X"))
+        self.label_3.setText(_translate("Dimensionador", "B"))
+        self.label.setText(_translate("Dimensionador", "A"))
+
--- IG/GUI.py
+++ IG/GUI.py
@@ -8,23 +8,44 @@ Created on Sat Oct 13 18:26:29 2018
 import sys
 from PyQt5 import uic, QtWidgets
 from VentanaIngresoDatos import Ui_IngresoDatos
+from Dimensionador import Ui_Dimensionador
 
 qtCreatorFile = "VentanaPrincipal.ui" # Nombre del archivo aquí.
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
-class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+class MyApp(QtWidgets.QMainWindow,Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        self.ingresoDatos.clicked.connect(self.abrir)
+        self.ingresoDatos.clicked.connect(self.abrirIngresoDatos)
         
-    def abrir(self):
+    def abrirIngresoDatos(self):       
+        
+        ventanaIngreso = VentanaIngresoDatos(self)
+        ventanaIngreso._init_()
+        
+        
+class VentanaIngresoDatos(QtWidgets.QMainWindow):
+    def _init_(self):
         self.ventana=QtWidgets.QMainWindow()
         self.ui=Ui_IngresoDatos()
         self.ui.setupUi(self.ventana)
-        self.ventana.show()
+        self.ventana.show()        
+        self.ui.BotonDimensionar.clicked.connect(self.abrirDimensionador)
+    
+    def abrirDimensionador(self):
+        ventanaDimensionador = VentanaDimensionador(self)
+        ventanaDimensionador._init_()
+        
+class VentanaDimensionador(QtWidgets.QMainWindow):        
+        def _init_(self):
+            self.ventana=QtWidgets.QMainWindow()
+            self.ui=Ui_Dimensionador()
+            self.ui.setupUi(self.ventana)
+            self.ventana.show()  
+     
 
 if __name__ == "__main__":
     app =  QtWidgets.QApplication(sys.argv)
